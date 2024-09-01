f1a451c024c90093233b80cae68a9a5659105023
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2024-09-01 20:58:21 +0530

    Initial Commit

---
+++ Multithreading-AsyncLet/Multithreading_AsyncLetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multithreading_AsyncLetApp.swift
+//  Multithreading-AsyncLet
+//
+//  Created by mac on 01/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Multithreading_AsyncLetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
